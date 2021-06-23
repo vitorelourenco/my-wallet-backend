295bd11b2652b2c220fba6240572cda28afb0bac
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-23 12:28:10 -0300

    Refactor routes

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/expenditures", (req, res) => getExpenditures(req, res));
-app.post("/expenditures", (req, res) => addExpenditure(req, res));
-
-app.get("/earnings", (req, res) => getEarnings(req, res));
-app.post("/earnings", (req, res) => addEarning(req, res));
-
 app.get("/statement", (req, res) => getStatement(req, res));
 
 app.post("/new/:logKind", (req, res) => postNewLog(req, res));
@@ -22,7 +16,6 @@ app.post("/new/:logKind", (req, res) => postNewLog(req, res));
 app.post("/signup", (req, res) => addUser(req, res));
 
 app.post("/login", (req, res) => login(req, res));
-
 
 const port = 4000;
 app.listen(port, () =>
