9c48302a8906880c558a5c52e1704dbc8343f908
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-24 09:48:03 -0300

    Add sessions route to check for active tokens

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ import { postNewLog, getLogs } from "./logs.js";
 import addUser from "./signup.js";
 import login from "./login.js";
 import logout from "./logout.js";
+import { confirmSession } from "./sessions.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get("/sessions/withtoken", (req, res) => confirmSession(req, res));
 
 app.get("/logs", (req, res) => getLogs(req, res));
 app.post("/logs/:logKind/new", (req, res) => postNewLog(req, res));
---
+++ sessions.js
@@ -0,0 +1,26 @@
+import errorWithStatus from "./errorWithStatus.js";
+import { authorizationSchema } from "./schemas.js";
+import connection from "./database.js";
+
+export async function confirmSession(req,res){
+  try {
+    const {value: authorization, error: authorizationError} = authorizationSchema.validate(req.headers.authorization);
+    if (authorizationError) throw new errorWithStatus(400);
+
+    const token = authorization.replace("Bearer ","");
+
+    const dbSession = await connection.query(`
+      SELECT *
+      FROM sessions
+      WHERE token = $1
+    ;`,[token]);
+
+    if (dbSession.rows.length !== 1) throw new errorWithStatus(401);
+
+    res.sendStatus(200);
+  } catch (err) {
+    if (err.status) res.sendStatus(err.status);
+    else res.sendStatus(500);
+    console.log(err);
+  }
+}
